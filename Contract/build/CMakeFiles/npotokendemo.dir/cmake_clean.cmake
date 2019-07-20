f25602f0a0f3030e64a966f1681e3432e6c3d8d8
lychees <lychees67@gmail.com>
2019-07-21 04:47:53 +0800

    Build

---
+++ Contract/build/CMakeFiles/npotokendemo.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/npotokendemo"
+  "CMakeFiles/npotokendemo-complete"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-install"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-mkdir"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-download"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-update"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-patch"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-configure"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-build"
+  "npotokendemo-prefix/src/npotokendemo-stamp/npotokendemo-test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/npotokendemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Contract/build/npotokendemo/CMakeFiles/npotokendemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/npotokendemo.dir/npotokendemo.cpp.obj"
+  "npotokendemo.pdb"
+  "npotokendemo.wasm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/npotokendemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
